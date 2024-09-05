41edc7ac932f8920d3e5266fb8d746e19407a445
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-09-05 08:45:14 +0700

    production

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"order-server/domain"
 	"order-server/services"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
@@ -31,30 +33,29 @@ func main() {
 
 	orderServer := services.NewOrderServer(db, levelDB)
 
-	orderServer.ProcessOrder("5m")
-	// c := cron.New(cron.WithSeconds())
-	// c.AddFunc("0 */5 * * * *", func() {
-	// 	now := time.Now()
-	// 	fmt.Println("Current time:", now.Format("2006-01-02 15:04:05"))
-	// 	time.Sleep(5 * time.Second)
-	// 	orderServer.ProcessOrder("5m")
-	// })
+	c := cron.New(cron.WithSeconds())
+	c.AddFunc("0 */5 * * * *", func() {
+		now := time.Now()
+		fmt.Println("Current time:", now.Format("2006-01-02 15:04:05"))
+		time.Sleep(5 * time.Second)
+		orderServer.ProcessOrder("5m")
+	})
 
-	// c.AddFunc("0 */4 * * *", func() {
-	// 	time.Sleep(5 * time.Second)
-	// 	orderServer.ProcessOrder("4h")
-	// })
+	c.AddFunc("0 */4 * * *", func() {
+		time.Sleep(5 * time.Second)
+		orderServer.ProcessOrder("4h")
+	})
 
-	// c.AddFunc("0 7 * * *", func() {
-	// 	time.Sleep(5 * time.Second)
-	// 	orderServer.ProcessOrder("1d")
-	// })
+	c.AddFunc("0 7 * * *", func() {
+		time.Sleep(5 * time.Second)
+		orderServer.ProcessOrder("1d")
+	})
 
-	// // Start the cron scheduler
-	// c.Start()
+	// Start the cron scheduler
+	c.Start()
 
 	fmt.Println("Cron job scheduler started")
 
 	// Wait indefinitely
-	// select {}
+	select {}
 }
